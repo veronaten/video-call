61fca021e823f1bca5fc48efed5b4c28b05ee039
Veronika <verona.binko@gmail.com>
2022-02-03 01:57:48 +0300

    Feat: add section Statistics

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Conference from './components/conference/Conference';
+import Statistics from './components/statistics/Statistics';
 
 function App() {
   return (
     <div>
       <Header />
       <Conference />
+      <Statistics />
     </div>
   );
 }
---
+++ src/components/statistics/Statistics.js
@@ -0,0 +1,25 @@
+import "./Statistics.css";
+
+const Statistics = () => {
+  return (
+    <section>
+      <h2>Recent Calls</h2>
+      <div className="statistics__wrapper">
+        <div className="statistics__header">
+          <div>#</div>
+          <div>Data</div>
+          <div>Started</div>
+          <div>Ended</div>
+          <div>Duration</div>
+          <div>Icon</div>
+        </div>
+
+        <div className="">
+         
+        </div> 
+      </div>
+    </section>
+  )
+};
+
+export default Statistics;
